fca55f5eab5e42661c49707d5b6dd1aba92c4b87
Venkatesh <Venkatesh@192.168.1.148>
2018-08-22 14:28:06 +0530

    Created new Java Project. Also, created folder structure to read data from Test_Data excel sheet.

---
+++ SeleniumCompleteAutomation/src/test_Configurations/readDataFromTestDataSheet.java
@@ -0,0 +1,53 @@
+package test_Configurations;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class readDataFromTestDataSheet 
+{
+	public static String ValueFromExcelSheet;
+	
+	public static String readDataFromTestDataSheet()
+	{
+
+		int CloumnNumber=1, RowNumber=1;
+		
+		
+		Logger logger = Logger.getLogger("readDataFromTestDataSheet");	
+		PropertyConfigurator.configure("./src/test_Logs/log4j.properties");
+		
+		File TestDataExcelSheetPath = new File("H:\\Software_Testing\\Test_Automation\\Selenium\\SeleniumTestAutomation\\Git\\Repository\\SeleniumCompleteAutomation\\src\\test_Data\\test_data.xlsx");
+		
+		try 
+		{
+			Workbook ExcelWorkBook = Workbook.getWorkbook(TestDataExcelSheetPath);
+			logger.info("Opened ExcelWorkBook...");
+			Sheet ExcelSheet = ExcelWorkBook.getSheet("Test_Data");
+			logger.info("Opened ExcelSheet...");
+	        Cell ExcelSheetCell = ExcelSheet.getCell(CloumnNumber,RowNumber);
+	        logger.info("Copying content from Cell ("+CloumnNumber+","+RowNumber+") [Foramt : Column, Row]...");
+	        ValueFromExcelSheet = ExcelSheetCell.getContents();
+	        logger.info("Cell Content = "+ValueFromExcelSheet);	        
+		}
+		catch (BiffException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return ValueFromExcelSheet;
+		
+	}
+
+}
---
+++ SeleniumCompleteAutomation/src/test_Configurations/testcases_Executer.java
@@ -0,0 +1,12 @@
+package test_Configurations;
+
+public class testcases_Executer 
+{
+
+	public static void main(String[] args) 
+	{
+		readDataFromTestDataSheet readDataFromTestDataSheetObject = new readDataFromTestDataSheet();
+		readDataFromTestDataSheetObject.readDataFromTestDataSheet();
+	}
+
+}
